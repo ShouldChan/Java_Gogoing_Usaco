26b92b432c178570e9302629c0ade4f08da47146
ShouldChan <ezanmydream@163.com>
2017-04-11 21:39:52 +0800

    first

---
+++ src/Ride.java
@@ -0,0 +1,26 @@
+/**
+ * ID: ezanmyd1
+ * LANG: JAVA
+ * TASK: test
+ * From: Section 1.1
+ * Created by Administrator on 2017/4/10.
+ */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+public class Ride {
+    public static final String IN_FILE = "./in_file/";
+    public static final String OUT_FILE = "./out_file/";
+
+    public int charReplace(String str) {
+        int num = 0;
+        BufferedReader f = new BufferedReader(new FileReader(In_FILE + "ride.in"));
+        return num;
+    }
+
+    public static void main(String[] args) throws IOExceptionEx {
+
+    }
+}
