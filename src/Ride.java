763650c8e2a9b59f19359755aa405792369bdd68
ShouldChan <ezanmydream@163.com>
2017-04-22 14:27:11 +0800

    ride_over

--- src/Ride.java
+++ src/Ride.java
@@ -6,10 +6,6 @@
  * Created by Administrator on 2017/4/10.
  */
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.*;
 import java.util.*;
 
@@ -17,69 +13,81 @@ import java.util.*;
 public class Ride {
     public static final String IN_FILE = "./in_file/";
     public static final String OUT_FILE = "./out_file/";
-    public static String readTxt(){
-        StringBuffer sb=new StringBuffer();
-        String line=null;
-        try{
-            File file=new File(IN_FILE);
-            if(!file.exists())
-                    throw new FileNotFoundException();
-            FileInputStream fis=new FileInputStream(file);
-            BufferedReader br=new BufferedReader(new InputStreamReader(fis));
-            line=br.readLine();
-            while(line!=null){
-                line=br.readLine();
-            }
-        }catch(Exception ex){
-            ex.printStackTrace();
-        }
-        System.out.println(sb.toString());
-        return sb.toString();
-    }
+    public static final Map<Character, Integer> map = new HashMap<Character, Integer>();
+    public static final List<String> lst = new ArrayList<String>();
 
-//    public int char2num(String str){
-//    return 0;
-//    }
-//    public int charReplace(String str) {
-//        int num = 0;
-////        BufferedReader f = new BufferedReader(new FileReader(IN_FILE + "ride.in"));
-//        return num;
-//    }
-
-    public static void main(String[] args) throws IOException {
-        Map<Character,Integer> map =new HashMap<Character,Integer>();
-//        map.put('A',1);
-        int num=1;
-        for(char letter='A';letter<='Z';letter++){
-
-            map.put(letter,num);
+    public static void saveMap() {
+        int num = 1;
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            map.put(letter, num);
             num++;
         }
         System.out.println(map);
-        BufferedReader f = new BufferedReader(new FileReader(IN_FILE + "ride.in"));
-        StringTokenizer st = new StringTokenizer(f.readLine(),"\n");
-        String s1=st.nextToken();
-        System.out.println(s1);
+    }
 
-        int i=0;
-        System.out.println(s1.length());
-        int acc=1;
-        while(i<s1.length()){
-            int x = map.get(s1.charAt(i));
-            i++;
-            System.out.println(x);
-            acc*=x;
-            System.out.println(acc);
+    public static void readTxt() {
+        String line = null;
+        try {
+            File file = new File(IN_FILE + "ride.in");
+            if (!file.exists())
+                throw new FileNotFoundException();
+            FileInputStream in = new FileInputStream(file);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            do {
+                line = br.readLine();
+                lst.add(line);
+//                System.out.println(line);
+            } while (line != null);
+
+//            kick the null out
+            lst.remove(null);
+            System.out.println(lst);
+            in.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        int result=0;
-        result=acc%47;
-        System.out.println(result);
-        System.out.println(s1);
 
+    }
+
+    public static void computeAndsave() {
+        int j = 0;
+        int[] result = new int[2];
+        try {
+            File file = new File(OUT_FILE + "ride.out");
+            if (!file.exists())
+                file.createNewFile();
+            FileOutputStream out = new FileOutputStream(file, false);
+            for (String s : lst) {
+                int acc = 1;
+                System.out.println(s);
+                for (int i = 0; i < s.length(); i++) {
+                    int x = map.get(s.charAt(i));
+                    acc *= x;
+                    System.out.println(acc);
+                }
+                result[j] = acc % 47;
+//                System.out.println(j);
+                System.out.println(result[j]);
+                j++;
+            }
+            StringBuffer sb = new StringBuffer();
+            if (result[0] == result[1]) {
+                sb.append("GO");
+                out.write(sb.toString().getBytes("utf-8"));
+            } else {
+                sb.append("STAY");
+                out.write(sb.toString().getBytes("utf-8"));
+            }
+            out.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        saveMap();
         readTxt();
-//        while(st.hasMoreElements()){
-//            System.out.println(st.nextToken());
-//        }
-
+        computeAndsave();
     }
 }
