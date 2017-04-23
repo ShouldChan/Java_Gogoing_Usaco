2fb870d2801d73e1932e2406099a4f4ca8fb28eb
ShouldChan <ezanmydream@163.com>
2017-04-23 12:58:30 +0800

    Gift_1

---
+++ src/Gift.java
@@ -0,0 +1,11 @@
+/**
+ * ID: ezanmyd1
+ * LANG: JAVA
+ * TASK: Greedy Gift Givers
+ * From: Section 1.1
+ * Created by Administrator on 2017/4/22.
+ */
+public class Gift {
+    public static final String IN_FILE = "./in_file/";
+    public static final String OUT_FILE = "./out_file/";
+}
